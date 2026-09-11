01aeab6ef10969f95f6e844ce16b5257c7230fb7
Bapan De <debapan2002@gmail.com>
2026-09-11 21:11:02 +0530

    api version v1

--- routes/api.php
+++ routes/api.php
@@ -19,26 +19,26 @@ Route::post('register', [AuthController::class,'register']);
 Route::post('login',[AuthController::class,'login']);
 
 Route::middleware('auth:sanctum')->group(function(){
+    Route::prefix('v1')->group(function(){
+        Route::get('/user',[AuthController::class, 'user']);
+        ROute::post('/logout',[AuthController::class, 'logout']);
+        Route::apiResource('authors', AuthorController::class);
+        Route::apiResource('books', BookController::class);
+        Route::apiResource('members', MemberController::class);
+        Route::apiResource('borrowings',BorrowingController::class)->only(['index','store','show']);
+        Route::post('/borrowings/{borrowing}/return', [BorrowingController::class,'returnBook']);
+        Route::get('/borrowings/overdue/list',[BorrowingController::class, 'overDue']);
 
-    Route::get('/user',[AuthController::class, 'user']);
-    ROute::post('/logout',[AuthController::class, 'logout']);
-    Route::apiResource('authors', AuthorController::class);
-    Route::apiResource('books', BookController::class);
-    Route::apiResource('members', MemberController::class);
-    Route::apiResource('borrowings',BorrowingController::class)->only(['index','store','show']);
-    Route::post('/borrowings/{borrowing}/return', [BorrowingController::class,'returnBook']);
-    Route::get('/borrowings/overdue/list',[BorrowingController::class, 'overDue']);
-
-    Route::get('statices',function(){
-        return response()->json([
-            'total_books'=>App\Models\Book::count(),
-            'total_authors'=>App\Models\Author::count(),
-            'total_member'=>App\Models\Member::count(),
-            'total_borrowed'=>App\Models\Borrowing::where('status','borrowed')->count(),
-            'total_borrowing'=>App\Models\Borrowing::where('status','overdue')->count(),
-        ]);
+        Route::get('statices',function(){
+            return response()->json([
+                'total_books'=>App\Models\Book::count(),
+                'total_authors'=>App\Models\Author::count(),
+                'total_member'=>App\Models\Member::count(),
+                'total_borrowed'=>App\Models\Borrowing::where('status','borrowed')->count(),
+                'total_borrowing'=>App\Models\Borrowing::where('status','overdue')->count(),
+            ]);
+        });
     });
-
 });
 
 
